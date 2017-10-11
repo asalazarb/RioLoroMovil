65400174eb11144820c4bb90b6120ada1990173a
Jose <jrobertoortiz112@gmail.com>
2017-10-11 07:25:35 -0600

    Versión 2 del listado de especies y visualización individual
    Se muestra el listado de especies y se puede acceder a ellas, presentan
    un par de problemas como el tamaño de cada item de la lista así como la
    visualización

---
+++ RioLoro/app/src/main/java/com/example/andres/rioloro/data/ListItem.java
@@ -0,0 +1,45 @@
+package com.example.andres.rioloro.data;
+
+/**
+ * Created by usuario on 10/10/2017.
+ */
+
+public class ListItem {
+    private String dateAndTime;
+    private String message;
+    private int colorResource;
+
+    /*It's common for an "Item" to have a unique Id for storing an a Database
+    private String uniqueIdentifier;*/
+
+    public ListItem(String dateAndTime, String message, int colorResource) {
+        this.dateAndTime = dateAndTime;
+        this.message = message;
+        this.colorResource = colorResource;
+    }
+
+    public int getColorResource() {
+        return colorResource;
+    }
+
+    public void setColorResource(int colorResource) {
+        this.colorResource = colorResource;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
